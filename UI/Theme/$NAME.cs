4a901cf68ce64df0b1a9976d9ce8400ad7a9c612
Adrian Preuß <adrian.preuss@icloud.com>
2019-02-14 22:36:02 +0100

    Adding initial files

---
+++ UI/Theme/$NAME.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DSTEd.UI.Theme {
+    public class $NAME : Xceed.Wpf.AvalonDock.Themes.Theme {
+        public override Uri GetResourceUri() {
+            return new Uri("/DSTEd;component/UI/Theme/$NAME.xaml", UriKind.Relative);
+        }
+    }
+}
